88358a9a63aa7379364f409c28d9c7a90535664c
Ricky Kim <rickykim@Rickys-MacBook-Pro.local>
2019-01-05 13:31:38 +0000

    ..

--- run_classifier.py
+++ run_classifier.py
@@ -431,7 +431,7 @@ class TweetProcessor(DataProcessor):
             #     text_a = tokenization.convert_to_unicode(line[1])
             #     label = "0"
             # else:
-            text_a = tokenization.convert_to_unicode(pre_processing(line[3]))
+            text_a = tokenization.convert_to_unicode(pre_processing(line[2]))
             label = tokenization.convert_to_unicode(pre_processing(line[1]))
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
