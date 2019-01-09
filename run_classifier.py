2cc8c1c5c06a171d5927f47bf8e3cde69708a377
Ricky Kim <rickykim@Rickys-MacBook-Pro.local>
2019-01-09 04:58:45 +0000

    ..

--- run_classifier.py
+++ run_classifier.py
@@ -395,10 +395,10 @@ class TweetProcessor(DataProcessor):
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
 
-    # def get_test_examples(self, data_dir):
-    #     """See base class."""
-    #     return self._create_examples(
-    #         self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
     def get_labels(self):
         """See base class."""
